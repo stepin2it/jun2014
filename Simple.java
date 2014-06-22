ae6cd961af281211aaf2ef19087add5527fc2946
StepIn2It <stepin2itinc@gmail.com>
2014-06-22 13:08:32 -0400

    First commit of Simple.java

---
+++ Simple.java
@@ -0,0 +1,13 @@
+public class Simple
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Simple java program");
+		int a = 1;
+		int b = 1;
+		int c = a+b;
+		System.out.println("The result of 1+1 is " + c);
+
+	}
+}
