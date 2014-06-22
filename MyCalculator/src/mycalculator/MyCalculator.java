b8fac23637602a97c342fef0491c470db47e6083
StepIn2It <stepin2itinc@gmail.com>
2014-06-22 16:34:32 -0400

    Added MyCalculator to repo

---
+++ MyCalculator/src/mycalculator/MyCalculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mycalculator;
+
+/**
+ *
+ * @author Stepin2
+ */
+public class MyCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to MyCalculator");
+        CalculatorForm form = new CalculatorForm();
+        form.setVisible(true);
+    }
+    
+}
